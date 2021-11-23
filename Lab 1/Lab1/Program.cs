7ce5f39e7cb8c0a53b9c9d0af75365f1daa5bf3e
masonhale123 <93341155+masonhale123@users.noreply.github.com>
2021-11-23 17:36:41 -0500

    Add files via upload

---
+++ Lab 1/Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Grading ID R1738
+   Lab 1 08/30/2020
+   CIS 199-01
+   This application prints a short description of my grading ID, hobbies, and my favorite book/movie */
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Grading ID:     R1738");
+            System.Console.WriteLine("Hobbies:        Video Games and working out");
+            System.Console.WriteLine("Favorite Book:  A Song of Ice and Fire");
+            System.Console.WriteLine("Favorite Movie: Django Unchained");
+            
+        }
+    }
+}
